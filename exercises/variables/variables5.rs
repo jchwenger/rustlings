f28d3cab43439d425598485c5b3111d5f720c769
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-13 22:35:32 +0100

    variables

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,16 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
 
-fn main() {
+use std::num::ParseIntError;
+
+fn main() -> Result<(), ParseIntError> {
     let number = "3"; // don't change this line
     println!("Number {}", number);
-    number = 3;
-    println!("Number {}", number);
+    let num2 = match number.parse::<u8>() {
+        Ok(num) => num,
+        Err(e) => return Err(e),
+    };
+    println!("Number {}", num2);
+    Ok(())
 }
