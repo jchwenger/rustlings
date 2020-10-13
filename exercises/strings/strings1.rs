727371f46bfa0c27ff080d3b8c39056e2e51c51a
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-13 22:56:27 +0100

    strings

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,13 +2,29 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-// I AM NOT DONE
-
+// https://stackoverflow.com/a/43080280
+// https://stackoverflow.com/a/24159933
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
+
+    let other = current_favorite_static_color();
+    println!("My current favorite static color is {}", other);
+
+    let sth = String::from("blah!");
+    println!("Something else I'd like to say: {}.", sth);
+
+    let s: &str = &sth[..]; // full slice of sth
+    println!("Almost the same: {}.", s);
+
+    let s_moa: &str = &*sth; // full slice of sth
+    println!("Yet again: {}.", s_moa);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
+}
+
+fn current_favorite_static_color() -> &'static str {
+    "green"
 }
