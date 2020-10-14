d00c30edaca918e1dfae99434534109e8d1b2b48
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-14 09:33:54 +0100

    fix in variables

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -7,10 +7,15 @@ use std::num::ParseIntError;
 fn main() -> Result<(), ParseIntError> {
     let number = "3"; // don't change this line
     println!("Number {}", number);
-    let num2 = match number.parse::<u8>() {
-        Ok(num) => num,
-        Err(e) => return Err(e),
-    };
-    println!("Number {}", num2);
+
+    // // very nice but what this exo is about
+    // let num2 = match number.parse::<u8>() {
+    //     Ok(num) => num,
+    //     Err(e) => return Err(e),
+    // };
+    // println!("Number {}", num2);
+
+    let number = 3;
+    println!("Number {}", number);
     Ok(())
 }
